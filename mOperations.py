b4cd7e7ae18ff37124615d95ed84b626d2940241
sahilnik1504 <sahilnikam36@gmail.com>
2026-08-31 19:43:27 +0530

    Initial commit

---
+++ mOperations.py
@@ -0,0 +1,24 @@
+# Define two numbers
+a = 15
+b = 4
+
+# Addition (+)
+print(f"Addition: {a} + {b} = {a + b}")
+
+# Subtraction (-)
+print(f"Subtraction: {a} - {b} = {a - b}")
+
+# Multiplication (*)
+print(f"Multiplication: {a} * {b} = {a * b}")
+
+# Division (/)
+print(f"Division: {a} / {b} = {a / b}")
+
+# Floor Division (//)
+print(f"Floor Division: {a} // {b} = {a // b}")
+
+# Modulo (%)
+print(f"Modulo: {a} % {b} = {a % b}")
+
+# Exponentiation (**)
+print(f"Exponentiation: {a} ** {b} = {a ** b}")
